8c95a15224bb6a116ed8d95d371dae06053430c7
ChuongNT <chuongnt_tts@rikkeisoft.com>
2023-10-24 08:47:16 +0700

    demo escape()

--- Python/escape_html.py
+++ Python/escape_html.py
@@ -1,8 +1,7 @@
-from markupsafe import escape
-from flask import Flask
+from flask import escape, request, Flask
 
-app = Flask(__name__)
-
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route('/some_route', methods=['POST'])
+def some_route():
+    user_input = request.form['user_input']
+    escaped_input = escape(user_input)
+    return f'You entered: {escaped_input}'
