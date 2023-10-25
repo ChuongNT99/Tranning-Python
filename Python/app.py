af38c3109557013fc3e0249d0ac132d551e26cdb
ChuongNT <chuongnt_tts@rikkeisoft.com>
2023-10-25 08:42:56 +0700

    update demo

--- Python/app.py
+++ Python/app.py
@@ -3,45 +3,40 @@ import os
 
 app = Flask(__name__)
 
-# Thư mục để lưu trữ các tệp tải lên
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024  # Giới hạn kích thước file lên tối đa là 100 KB
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Giới hạn kích thước tệp lên tối đa là 16 MB
 
+def allowed_file(filename):
+    allowed_extensions = {'jpg', 'jpeg', 'png', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1] in allowed_extensions
 
 @app.route('/')
 def index():
     return render_template('file_upload.html')
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in {'jpg', 'jpeg', 'png', 'gif'}
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # Kiểm tra xem yêu cầu có chứa tệp tải lên hay không
-    if 'file' not in request.files:
-        return 'No file part'
-
     file = request.files['file']
 
     # Kiểm tra xem tệp đã chọn có tên không trống
     if file.filename == '':
         return 'No selected file'
 
-    # Kiểm tra xem thư mục UPLOAD_FOLDER đã tồn tại chưa
+    # Kiểm tra kích thước tệp
+    if len(file.read()) > app.config['MAX_CONTENT_LENGTH']:
+        return 'File size exceeds the allowed limit'
+
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
 
-    # Kiểm tra xem tệp là hợp lệ
     if file and allowed_file(file.filename):
-        # Kiểm tra có quá dung lượng tối đa hay không
-        if file.content_length <= app.config['MAX_CONTENT_LENGTH']:
-            # Lưu tệp tải lên vào thư mục lưu trữ
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            return 'File uploaded successfully'
-        else:
-            return 'File size exceeds the allowed limit'
-    return 'Invalid file or no file provided'
+        # Xử lý tải lên tệp ở đây
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return 'File uploaded successfully'
+    else:
+        return "Invalid file. Only files with the extension jpg, jpeg, png or gif are accepted."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
