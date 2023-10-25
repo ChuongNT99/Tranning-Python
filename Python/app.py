d29a1fa08bdc6119d386d70426d3306ad90c25a8
ChuongNT <chuongnt_tts@rikkeisoft.com>
2023-10-25 08:05:01 +0700

    file size limit

--- Python/app.py
+++ Python/app.py
@@ -6,10 +6,15 @@ app = Flask(__name__)
 # Thư mục để lưu trữ các tệp tải lên
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024  # Giới hạn kích thước file lên tối đa là 100 KB
+
 
 @app.route('/')
 def index():
     return render_template('file_upload.html')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in {'jpg', 'jpeg', 'png', 'gif'}
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -27,9 +32,16 @@ def upload_file():
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
 
-    # Lưu tệp tải lên vào thư mục lưu trữ
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-    return 'File uploaded successfully'
+    # Kiểm tra xem tệp là hợp lệ
+    if file and allowed_file(file.filename):
+        # Kiểm tra có quá dung lượng tối đa hay không
+        if file.content_length <= app.config['MAX_CONTENT_LENGTH']:
+            # Lưu tệp tải lên vào thư mục lưu trữ
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return 'File uploaded successfully'
+        else:
+            return 'File size exceeds the allowed limit'
+    return 'Invalid file or no file provided'
 
 if __name__ == '__main__':
     app.run(debug=True)
