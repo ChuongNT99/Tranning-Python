f488d2192934072858f748ba621baec4b09294bf
ChuongNT <chuongnt_tts@rikkeisoft.com>
2023-10-26 10:02:46 +0700

    demo add extension HTTP_Auth

---
+++ Python/extensions.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from flask_httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": "hello",
+    "susan": "bye"
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and users[username] == password:
+        return username
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.username())
+
+if __name__ == '__main__':
+    app.run(debug=True)
